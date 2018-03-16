e4ff807c15d6d7a75addde756f474f226c5e73bf
dxcweb <dxcweb@qq.com>
2018-03-16 15:21:35 +0800

    no message

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -9,11 +9,11 @@ export interface BlockProps {
   /**
    * 水平方向 start（开始）center（居中）end（结束），around与between只能在 layout="horizontal"时有效。
    */
-  horizontal: 'start' | 'center' | 'end' | 'around' | 'between';
+  horizontal?: 'start' | 'center' | 'end' | 'around' | 'between';
   /**
    * 垂直方向 start（开始）center（居中）end（结束），around与between只能在 layout="vertical"时有效。
    */
-  vertical: 'start' | 'center' | 'end' | 'around' | 'between';
+  vertical?: 'start' | 'center' | 'end' | 'around' | 'between';
 }
 
 export default class Block extends React.Component<BlockProps, any> {}
